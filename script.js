bc7d1edf96fe3962e141155e89aba34c268e383a
jess4lynwang@gmail.com <jess4lynwang@gmail.com>
2020-08-20 18:16:09 -0700

    automating info dump done

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ function loadContent() {
         }
     };
     let name = location.search.substring(6);
-    xmlhttp.open("GET", "https://jessalynwang.github.io/articles/" + name + ".json", true);
+    xmlhttp.open("GET", "https://jessalynwang.github.io/educationatlas.github.io/internships/" + name + ".json", true);
+    xmlhttp.send();
+}
 
+function addContent(articleData) {
+    let place = document.getElementsByClassName("innerthings");
+    let content = "";
+    console.log(articleData);
+    for (let a in articleData) {
+        content = content + "<div class='info-blub'>" + articleData[a]["name"] + "<hr class='gold' />" + articleData[a]["summary"] + "<p>Eligibility:" + "<ul style='list-style-type: disc; padding-left: 50px;'>";
+        for (let c in articleData[a]["eligibility"]) {
+            content = content + articleData[a]["eligibility"][c];
+        }
+        content = content + "</ul>" + "</p>" + "<p>Application Components:" + "<ul style='list-style-type: disc; padding-left: 50px;'>";
+        for (let c in articleData[a]["components"]) {
+            content = content + articleData[a]["components"][c];
+        }
+        content = content + "</ul>" + "</p>" + "<p>Application Deadlines:" + "<ul style='list-style-type: disc; padding-left: 50px;'>";
+        for (let c in articleData[a]["deadlines"]) {
+            content = content + articleData[a]["deadlines"][c];
+        }
+        content = content + "</ul>" + "</p>" + "</div>";
+    }
+
+    for (let d in place) place[d].innerHTML = content;
 }
